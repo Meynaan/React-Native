4d414622e1a06697cdb564f1508b1576709b7ee1
Meynaan <meynaanjar@gmail.com>
2024-12-16 00:22:45 +0700

    Commit 7

--- CrudMahasiswaNav.js
+++ CrudMahasiswaNav.js
@@ -3,43 +3,31 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Profil from './App';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faUser, faGear, faCirclePlus, faUserPen, faMap, faHouse } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUser, faGear, faCirclePlus, faUserPen, faMap, faHouse } from '@fortawesome/free-solid-svg-icons';
 import { WebView } from 'react-native-webview';
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Createdata from './Createdata';
-import Datamahasiswa from './Listdata'
-import Editdata from './Editdata'
+import Datamahasiswa from './Listdata';
+import Editdata from './Editdata';
 
 function ProfilScreen() {
-  return (
-    <Profil />
-  );
+  return <Profil />;
 }
 function HomeScreen() {
-  return (
-    <Createdata />
-  );
+  return <Createdata />;
 }
 
 function DataMahasiswaScreen() {
-  return (
-    < Datamahasiswa />
-  );
+  return <Datamahasiswa />;
 }
 
 function EditScreen() {
-  return (
-    < Editdata />
-  );
+  return <Editdata />;
 }
 
 function WebScreen() {
-  return (
-    <WebView
-      source={{ uri: 'https://leaflet-app-dusky.vercel.app/home' }}
-    />
-  )
+  return <WebView source={{ uri: 'https://leaflet-app-dusky.vercel.app/home' }} />;
 }
 
 const Tab = createBottomTabNavigator();
@@ -47,51 +35,68 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={ProfilScreen}
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: 5, // Jarak dari bawah layar
+            left: 20, // Jarak dari kiri
+            right: 20, // Jarak dari kanan
+            borderRadius: 15, // Membuat sudut membulat
+            height: 70, // Tinggi bar
+            backgroundColor: 'white', // Warna latar belakang
+            shadowColor: '#000', // Warna bayangan
+            shadowOffset: { width: 0, height: 5 }, // Posisi bayangan
+            shadowOpacity: 0.1, // Transparansi bayangan
+            shadowRadius: 10, // Radius bayangan
+            elevation: 10, // Bayangan untuk Android
+          },
+          tabBarItemStyle: {
+            margin: 5, // Jarak antar ikon
+          },
+          tabBarLabelStyle: {
+            fontSize: 12, // Ukuran teks label
+          },
+          tabBarActiveTintColor: '#AB4459', // Warna ikon aktif
+          tabBarInactiveTintColor: '#8e8e93', // Warna ikon tidak aktif
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={ProfilScreen}
           options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesomeIcon icon={faHouse} color={color} size={20} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faHouse} color={color} size={20} />,
           }}
         />
-        <Tab.Screen name="Daftar KK" component={DataMahasiswaScreen}
+        <Tab.Screen
+          name="Daftar KK"
+          component={DataMahasiswaScreen}
           options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesomeIcon icon={faUser} color={color} size={20} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faUser} color={color} size={20} />,
           }}
         />
-        <Tab.Screen name="Tambah" component={HomeScreen}
+        <Tab.Screen
+          name="Tambah"
+          component={HomeScreen}
           options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesomeIcon icon={faCirclePlus} color={color} size={20} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faCirclePlus} color={color} size={20} />,
           }}
         />
-
-        <Tab.Screen name="Edit" component={EditScreen}
+        <Tab.Screen
+          name="Edit"
+          component={EditScreen}
           options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesomeIcon icon={faUserPen} color={color} size={20} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faUserPen} color={color} size={20} />,
           }}
         />
-
-        <Tab.Screen name="Map" component={WebScreen}
+        <Tab.Screen
+          name="Map"
+          component={WebScreen}
           options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesomeIcon icon={faMap} color={color} size={20} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faMap} color={color} size={20} />,
           }}
         />
-
-
       </Tab.Navigator>
     </NavigationContainer>
   );
